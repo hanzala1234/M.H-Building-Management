49239fbeda588e2497c8ef221e3ee1f0b12edbc6
hanzala1234 <muhammadhanzala12@gmail.com>
2019-01-26 18:51:27 +0500

    Dockerfile has been added for server services

--- server/message-service/config/config.js
+++ server/message-service/config/config.js
@@ -1,4 +1,7 @@
 exports=module.exports=function(app){
+	if(process.env.phase=='production')
+app.set('mongodb-url','mongodb://message-database:27017/Building-Management-Messages');
+	else
 app.set('mongodb-url','mongodb://localhost:27017/Building-Management-Messages');
 app.set('app-port','8020');
 app.set('members-name-url','http://localhost:8080/member/getMultiple');
